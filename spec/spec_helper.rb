e8fdfeaf4cfb53a50cba0ad0bc0db10036104c92
RamilGilmanov <ramil.gilmanov@flatstack.com>
2016-01-26 19:16:15 +0300

    Add notifier spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require File.expand_path("../../spec/dummy/config/environment.rb", __FILE__)
 require_relative "../lib/ssl_reminder"
+require "factory_girl_rails"
+require "database_cleaner"
+require "ffaker"
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
